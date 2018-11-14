c4e33aaefdeae1f01f3048fa11c433d40c934cee
Becca Elenzil <becca.elenzil@bush.edu>
2018-11-14 11:32:51 -0800

    added video

--- computer-art/rotating-images/rotate.js
+++ computer-art/rotating-images/rotate.js
@@ -1,21 +1,28 @@
 var img;
 var offset = 0;
 var easing = 0.05;
+var vid;
 
 function preload(){
   img = loadImage("moonwalk.jpg");  // Load an image into the program
+  vid = createVideo(["light.m4v"]);
+  
 }
 
 function setup() {
   createCanvas(800, 400, WEBGL);
+  vid.elt.muted = true;
+  vid.loop();
+  vid.hide();
 }
 
 function draw() {
   background(0)
 
+  texture(img,0,0);
   rectMode(CENTER)
   rect(0,0,800,400)
-  texture(img,0,0);
+  
 
   push()
   rotateX(frameCount*0.01)
@@ -24,12 +31,12 @@ function draw() {
   texture(img,0,0);
   
   //translate(100,100,100)
-  rotateY(frameCount*0.01)
-  rotateX(frameCount*0.01)
-  rotateZ(frameCount*0.01)
+  rotateY(frameCount*0.005)
+  rotateX(frameCount*0.005)
+  rotateZ(frameCount*0.005)
   
-  texture(img,0,0);  // Display at full opacity
-  box(100,100,100)
+  texture(vid,0,0);  // Display at full opacity
+  box(150,150,150)
   
   pop()
 
