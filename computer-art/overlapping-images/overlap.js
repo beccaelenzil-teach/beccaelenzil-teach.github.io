4ef2d94fd15a63d0b75d0df00c141b91dce1e6f2
Becca Elenzil <becca.elenzil@bush.edu>
2018-11-14 10:33:06 -0800

    rotation

--- computer-art/overlapping-images/overlap.js
+++ computer-art/overlapping-images/overlap.js
@@ -7,14 +7,35 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(800, 400, WEBGL);
 }
 
 function draw() {
-  //background(0)
-  image(img, 0, 0);  // Display at full opacity
-  var dx = (mouseX-img.width/2) - offset;
-  offset += dx * easing;
-  tint(255, 127);  // Display at half opacity
-  image(img, offset, 0);
+  background(0)
+
+  rectMode(CENTER)
+  rect(0,0,800,400)
+  texture(img,0,0);
+
+  push()
+  rotateX(frameCount*0.01)
+  rectMode(CENTER)
+  rect(0,0,800,400)
+  texture(img,0,0);
+  
+  //translate(100,100,100)
+  rotateY(frameCount*0.01)
+  rotateX(frameCount*0.01)
+  rotateZ(frameCount*0.01)
+  
+  texture(img,0,0);  // Display at full opacity
+  box(100,100,100)
+  
+  pop()
+
+  ////overlaping code
+  //var dx = (mouseX-img.width/2) - offset;
+  //offset += dx * easing;
+  //tint(255, 127);  // Display at half opacity
+  //texture(img, offset, 0);
 }
